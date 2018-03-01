c679dc8029f314b59c789c2ed9e4245a269143d5
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-03-01 15:48:26 +0530

    Added base version with SASS, fonts , and more changes

--- src/components/Landing/Contact.js
+++ src/components/Landing/Contact.js
@@ -1,15 +1,21 @@
 import React from "react";
 const Contact = () => {
+  const style = {
+    color: "blue"
+  };
+
+  const contactInfo = "You can also send me an email @ abhinav_m@hotmail.com";
+
   return (
     <div className="socialIcons">
       <h3>Contact me</h3>
       <hr />
       <div className="icons">
-        <i style={{ color: "blue" }} className="fab fa-linkedin" />
+        <i style={style} className="fab fa-linkedin" />
         <i className="fab fa-github-square" />
         <i className="fab fa-codepen" />
       </div>
-      <p> You can also send me an email @ abhinav_m@hotmail.com</p>
+      <p> {contactInfo}</p>
     </div>
   );
 };
