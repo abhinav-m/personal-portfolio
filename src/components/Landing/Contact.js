5ccfb4f2c893225259f45b1dcad50730b1ff83ec
Abhinav mishra <abhinav.thinktank@gmail.com>
2018-10-21 15:15:33 +0530

    Added code in personal portfolio

--- src/components/Landing/Contact.js
+++ src/components/Landing/Contact.js
@@ -11,18 +11,20 @@ const Contact = () => {
       <h3>Contact me</h3>
       <hr />
       <div className="icons">
-        <i style={style} className="fab fa-linkedin">
-          <a
-            href="https://www.linkedin.com/in/abhinav-mishra-78228a119/"
-            target="_blank"
-          />
-        </i>
-        <i className="fab fa-github-square">
-          <a href="https://github.com/abhinav-m" target="_blank" />
-        </i>
-        <i className="fab fa-codepen">
-          <a href="https://codepen.io/abhinavthinktank/" target="_blank" />
-        </i>
+        <a
+          href="https://www.linkedin.com/in/abhinav-mishra-78228a119/"
+          target="_blank"
+        >
+          <i style={style} className="fab fa-linkedin" />
+        </a>
+
+        <a href="https://github.com/abhinav-m" target="_blank">
+          <i className="fab fa-github-square" />
+        </a>
+
+        <a href="https://codepen.io/abhinavthinktank/" target="_blank">
+          <i className="fab fa-codepen" />
+        </a>
       </div>
       <p> {contactInfo}</p>
     </div>
