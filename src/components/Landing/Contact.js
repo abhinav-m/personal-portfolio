3797b6f60849a8e577216788bf764954afc78ff6
Abhinav mishra <abhinav.thinktank@gmail.com>
2018-10-21 14:55:06 +0530

    Added code for Simon game built using html css and jquery

--- src/components/Landing/Contact.js
+++ src/components/Landing/Contact.js
@@ -11,9 +11,18 @@ const Contact = () => {
       <h3>Contact me</h3>
       <hr />
       <div className="icons">
-        <i style={style} className="fab fa-linkedin" />
-        <i className="fab fa-github-square" />
-        <i className="fab fa-codepen" />
+        <i style={style} className="fab fa-linkedin">
+          <a
+            href="https://www.linkedin.com/in/abhinav-mishra-78228a119/"
+            target="_blank"
+          />
+        </i>
+        <i className="fab fa-github-square">
+          <a href="https://github.com/abhinav-m" target="_blank" />
+        </i>
+        <i className="fab fa-codepen">
+          <a href="https://codepen.io/abhinavthinktank/" target="_blank" />
+        </i>
       </div>
       <p> {contactInfo}</p>
     </div>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,74 +1,78 @@
-import React from 'react';
+import React from "react";
 
-import PortfolioCard from './PortfolioCard';
+import PortfolioCard from "./PortfolioCard";
 
 const projectsDone = [
   {
     projectName: "Buddy4Study's core website",
-    projectLink: 'https://www.buddy4study.com',
+    projectLink: "https://www.buddy4study.com",
     dataDescription:
-      'Developed and deployed some of the core features on www.buddy4study.com  a portal which connects scholarships with students'
+      "Developed and deployed some of the core features on www.buddy4study.com  a portal which connects scholarships with students"
   },
   {
-    projectName: 'Dungeon crawler game built using React.',
-    projectLink: 'https://react-dungeon-master.herokuapp.com/',
-    dataDescription: 'Dungeon crawler game built using React.js'
-  },
-  {
-    projectName: 'Simulation of game of life',
-    projectLink: 'https://codepen.io/abhinavthinktank/full/wqEPPN/',
+    projectName: "Simon game built using jquery,html and css",
+    projectLink: "https://codepen.io/abhinavthinktank/pen/RgmBjZ",
     dataDescription:
-      'Mathematical simulation of game of life built using React.js.'
+      " Developed and deployed Simon memory game using jquery, html and css."
   },
   {
-    projectName: 'Minesweeper',
-    projectLink: 'https://codepen.io/abhinavthinktank/pen/yXLGRP',
-    dataDescription: 'Minesweeper game built using jQuery, html and css'
+    projectName: "Dungeon crawler game built using React.",
+    projectLink: "https://react-dungeon-master.herokuapp.com/",
+    dataDescription: "Dungeon crawler game built using React.js"
   },
   {
-    projectName: 'Recipe box',
-    projectLink: 'https://codepen.io/abhinavthinktank/pen/mMOjJb',
-    dataDescription: 'Recipe box built using React.js,'
-  },
-  {
-    projectName: 'Calculator',
-    projectLink: 'https://codepen.io/abhinavthinktank/pen/BZwBVy',
-    dataDescription: 'Calculator built using jquery html and css'
-  },
-  {
-    projectName: 'Scatter plot graph',
-    projectLink: 'https://codepen.io/abhinavthinktank/pen/XaGQBr',
-    dataDescription: 'Scatter plot graph built using D3.js'
-  },
-  {
-    projectName: 'Tic Tac Toe',
-    projectLink: 'https://codepen.io/abhinavthinktank/pen/XaGQBr',
-    dataDescription: 'Tic Tac Toe game built using jquery , CSS , HTML'
-  },
-  {
-    projectName: 'Meteor data world map',
-    projectLink: 'https://codepen.io/abhinavthinktank/full/EQQdgM/',
-    dataDescription: 'Meteorite data across the globe'
-  },
-  {
-    projectName: 'Force directed graph ',
-    projectLink: 'https://codepen.io/abhinavthinktank/pen/QQagQb',
-    dataDescription: 'Force directed graph showing nation contiguity'
-  },
-  {
-    projectName: 'Scatter plot graph',
-    projectLink: 'https://codepen.io/abhinavthinktank/pen/XaGQBr',
+    projectName: "Simulation of game of life",
+    projectLink: "https://codepen.io/abhinavthinktank/full/wqEPPN/",
     dataDescription:
-      'Scatter plot graph built on doping in professional bicycle racing'
+      "Mathematical simulation of game of life built using React.js."
   },
   {
-    projectName:'React - drumkit',
-    projectLink:"https://codepen.io/abhinavthinktank/pen/NOzjQz",
-    dataDescription:"A drumkit built using React.js. "
+    projectName: "Minesweeper",
+    projectLink: "https://codepen.io/abhinavthinktank/pen/yXLGRP",
+    dataDescription: "Minesweeper game built using jQuery, html and css"
+  },
+  {
+    projectName: "Recipe box",
+    projectLink: "https://codepen.io/abhinavthinktank/pen/mMOjJb",
+    dataDescription: "Recipe box built using React.js,"
+  },
+  {
+    projectName: "Calculator",
+    projectLink: "https://codepen.io/abhinavthinktank/pen/BZwBVy",
+    dataDescription: "Calculator built using jquery html and css"
+  },
+  {
+    projectName: "Scatter plot graph",
+    projectLink: "https://codepen.io/abhinavthinktank/pen/XaGQBr",
+    dataDescription: "Scatter plot graph built using D3.js"
+  },
+  {
+    projectName: "Tic Tac Toe",
+    projectLink: "https://codepen.io/abhinavthinktank/pen/XaGQBr",
+    dataDescription: "Tic Tac Toe game built using jquery , CSS , HTML"
+  },
+  {
+    projectName: "Meteor data world map",
+    projectLink: "https://codepen.io/abhinavthinktank/full/EQQdgM/",
+    dataDescription: "Meteorite data across the globe"
+  },
+  {
+    projectName: "Force directed graph ",
+    projectLink: "https://codepen.io/abhinavthinktank/pen/QQagQb",
+    dataDescription: "Force directed graph showing nation contiguity"
+  },
+  {
+    projectName: "Scatter plot graph",
+    projectLink: "https://codepen.io/abhinavthinktank/pen/XaGQBr",
+    dataDescription:
+      "Scatter plot graph built on doping in professional bicycle racing"
+  },
+  {
+    projectName: "React - drumkit",
+    projectLink: "https://codepen.io/abhinavthinktank/pen/NOzjQz",
+    dataDescription: "A drumkit built using React.js. "
   }
 ];
-
-
 
 const PortfolioList = props => (
   <div className="portfolio-grid-container">
